03b3d8e09f8bbf1803058cf5ae15841de980f151
VadimG2 <152259897+VadimG2@users.noreply.github.com>
2024-01-04 23:17:01 +0300

    lesson 45 NO this one)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,41 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class Point
     {
-        static void Main(string[] args)
+        public int X;
+        public int Y;
+        public char sym;
+
+        public Point(int _x, int _y, char _sym)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
-            Console.ReadLine();
+            X = _x;
+            Y = _y;
+            sym = _sym;
         }
+       
+
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(sym);
+        }
+        
     }
 }
