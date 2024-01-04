54e8db3d6454dd02c2fc289a8d36328142e6787b
VadimM <vad.gvozdew@yandex.ru>
2024-01-05 00:15:06 +0300

    lesson6:class hor\vert.lines

---
+++ ConsoleApp1/ConsoleApp1/VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+        public VerticalLine(int x, int yLower, int yUpper, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yLower; y <= yUpper; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
